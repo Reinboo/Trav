e0ab1f95be94f915446899f273eb00a609f2b334
Reinboo <nivalis@protonmail.com>
2019-02-24 15:27:29 +0100

    QueryBreadcrumbs styling

--- src/js/components/presentational/QueryBreadcrumbs.jsx
+++ src/js/components/presentational/QueryBreadcrumbs.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 
 const QueryBreadcrumbs = ({ queries, handleClick }) => (
   <div>
     {queries.map((query, index) => (
       // eslint-disable-next-line react/no-array-index-key
-      <span onClick={handleClick} id={index} key={index}>{`${query}>`}</span>
+      <Breadcrumb onClick={handleClick} id={index} key={index}>
+        {`${query}`}
+      </Breadcrumb>
     ))}
   </div>
 );
@@ -15,5 +18,25 @@ QueryBreadcrumbs.propTypes = {
   queries: PropTypes.arrayOf(PropTypes.string).isRequired,
   handleClick: PropTypes.func.isRequired,
 };
+
+const Breadcrumb = styled.button`
+  position: relative;
+  background: none;
+  border: none;
+  margin-right: 1rem;
+  padding: 0;
+  text-decoration: underline;
+  
+  &:hover {
+    color: #00695c;
+    cursor: pointer;
+  }
+  
+  &::after {
+    content: '>';
+    position: absolute;
+    left: calc( 100% + .25rem );
+  }
+`;
 
 export default QueryBreadcrumbs;
